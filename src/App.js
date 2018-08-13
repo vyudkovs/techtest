5db46441cc65b0201525629240c1d9ef878d39bf
vadimvyudkovs@yahoo.com <vyudkovs@yahoo.com>
2018-08-12 21:30:27 -0600

    'round1'

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { result: {} };
+  }
   render() {
+  fetch(
+    'https://thewirecutter.com/wp-json/wp/v2/posts',
+    {
+      method: 'GET',
+      mode:'no-cors',
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {JSON.stringify(this.state.result)}
       </div>
     );
   }
